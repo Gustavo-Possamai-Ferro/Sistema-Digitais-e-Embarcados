069abac013f390e3716902bfa75fdf91c267e3a8
Gustavo-Possamai-Ferro <gustavo.243923@alunosatc.edu.br>
2025-06-03 15:10:55 -0300

    Update Atividade4(Semana13).ino

--- Arduino/Atividade4(Semana13).ino
+++ Arduino/Atividade4(Semana13).ino
@@ -1,7 +1,7 @@
 # define pin_led1 13
 # define pin_led2 12
-# define pin_led1 11
-# define pin_led2 10
+# define pin_led3 11
+# define pin_led4 10
 # define pin_botaoA 2
 # define pin_botaoB 3
 
@@ -18,19 +18,24 @@ void loop() {
   bool estado1 = digitalRead(pin_botaoA);
   bool estado2 = digitalRead(pin_botaoB);
 
-  if(estado1 & estado2 == 1) {
+  if(estado1 ==1 && estado2 == 1) {
     digitalWrite(pin_led1, 1);
   } else {
     digitalWrite(pin_led1, 0);
   }
-    if(estado1 || estado2 == 1) {
+    if(estado1 == 1 || estado2 == 1) {
     digitalWrite(pin_led2, 1);
   } else {
     digitalWrite(pin_led2, 0);
   }
-    if(estado1 ^ estado2 == 1) {
+    if(estado1 == 1 ^ estado2 == 1) {
     digitalWrite(pin_led3, 1);
   } else {
     digitalWrite(pin_led3, 0);
+  }
+      if(estado1 == 1 ^ estado2 == 1) {
+    digitalWrite(pin_led4, 1);
+  } else {
+    digitalWrite(pin_led4, 0);
   }
 }
