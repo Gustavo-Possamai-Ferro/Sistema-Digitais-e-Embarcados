0414d59da4d63e44ea0dac95005e417da9fb5ffa
Gustavo-Possamai-Ferro <gustavo.243923@alunosatc.edu.br>
2025-06-03 13:47:08 -0300

    Atividade4.ino

--- Arduino/Atividade4.ino
+++ Arduino/Atividade4.ino
@@ -2,21 +2,35 @@
 # define pin_led2 12
 # define pin_led1 11
 # define pin_led2 10
-# define pin_led1 3
-# define pin_led2 2
+# define pin_botaoA 2
+# define pin_botaoB 3
 
 void setup() {
   pinMode(pin_led1, OUTPUT);
   pinMode(pin_led2, OUTPUT);
   pinMode(pin_led3, OUTPUT);
   pinMode(pin_led4, OUTPUT);
-  pinMode(pin_botao1, INPUT);
-  pinMode(pin_botao2, INPUT);
+  pinMode(pin_botaoA, INPUT);
+  pinMode(pin_botaoB, INPUT);
 }
 
 void loop() {
-  bool estado1 = digitalRead(pin_botao1);
-  bool estado2 = digitalRead(pin_botao2);
+  bool estado1 = digitalRead(pin_botaoA);
+  bool estado2 = digitalRead(pin_botaoB);
 
-  if(estado1 & estado2 == 1)
+  if(estado1 & estado2 == 1) {
+    digitalWrite(pin_led1, 1);
+  } else {
+    digitalWrite(pin_led1, 0);
+  }
+    if(estado1 || estado2 == 1) {
+    digitalWrite(pin_led2, 1);
+  } else {
+    digitalWrite(pin_led2, 0);
+  }
+    if(estado1 ^ estado2 == 1) {
+    digitalWrite(pin_led3, 1);
+  } else {
+    digitalWrite(pin_led3, 0);
+  }
 }
