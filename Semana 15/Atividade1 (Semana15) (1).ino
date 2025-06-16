b6a755952c09221c221664bb4a2e41ba6a5a863e
Gustavo-Possamai-Ferro <gustavo.243923@alunosatc.edu.br>
2025-06-16 14:29:25 -0300

    Semana 15

---
+++ Semana 15/Atividade1 (Semana15) (1).ino
@@ -0,0 +1,36 @@
+# define pin_led1 13
+# define pin_led2 12
+# define pin_led3 11
+# define pin_led4 10
+# define pin_botaoA 2
+# define pin_botaoB 3
+int conta = 0;
+int soma = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(pin_led1, OUTPUT);
+  pinMode(pin_led2, OUTPUT);
+  pinMode(pin_led3, OUTPUT);
+  pinMode(pin_led4, OUTPUT);
+  pinMode(pin_botaoA, INPUT);
+  pinMode(pin_botaoB, INPUT);
+}
+
+void loop() {
+  bool estado1 = digitalRead(pin_botaoA);
+  bool estado2 = digitalRead(pin_botaoB);
+
+  if (estado1 == 1 && soma == 0 ) {
+    conta += 1;
+    soma += 1;
+  }
+  if (estado1 == 0 && estado2 == 0) {
+    soma = 0;
+  }
+  if (estado2 == 1 && soma == 0) {
+    conta -= 1;
+    soma += 1;
+  }
+  Serial.println(conta);
+
+}
